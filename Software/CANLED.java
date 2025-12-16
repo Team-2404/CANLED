14bf1f3d0f06cbef3057fbd3c3aaf57925a6358f
Conor Kelly Gerakos <125530751+ConorKellyGerakos@users.noreply.github.com>
2025-12-15 17:51:41 -0800

    Java lib tested, works w/ command based

--- Software/CANLED.java
+++ Software/CANLED.java
@@ -1,14 +1,14 @@
-package CANLED;
+package frc.robot.subsystems;
 //  By Conor Kelly Gerakos, Team 2404
 //  For usage with the CANLED board.
-
+//  Place into subsystems folder.
 
 import edu.wpi.first.wpilibj.CAN;
 
 public class CANLED{
     private int frcId;
-    private final int rainbow_wave = 1;
-    private final int rainbow_solid = 2;
+    private final int rainbow_wave = 4;
+    private final int rainbow_solid = 3;
     private final int apiId = 473;
     private CAN canled;
     public CANLED(int id){
@@ -21,18 +21,18 @@ public class CANLED{
         canled = new CAN(55);
     }
 
-    public void rainbowCycle(){
-        byte[] msg = {rainbow_wave};
+    public void rainbowCycle(int wait){
+        byte[] msg = {rainbow_wave,(byte)wait};
         canled.writePacket(msg, apiId);
     }
 
-    public void rainbowSolid(){
-        byte[] msg = {rainbow_solid};
+    public void rainbowSolid(int wait){
+        byte[] msg = {rainbow_solid, (byte)wait};
         canled.writePacket(msg, apiId);
     }
 
     public void setColor(byte r, byte g, byte b){
-        byte[] msg = {0, r, g, b};
+        byte[] msg = {1, r, g, b};
         canled.writePacket(msg, apiId);
     }
 }
